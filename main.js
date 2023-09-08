19665c6857504c485d72d03ab659870c4557f829
HicaroD <hdanrlley1@gmail.com>
2023-09-08 18:02:38 -0300

    chore: initial code for learning EJS

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import ejs from "ejs";
+
 function main() {
-  console.log("Hello, world!");
+  const layout = fs.readFileSync("./layouts/default.ejs").toString();
+
+  let renderedHTML = ejs.render(layout, {
+    site: { title: "website" },
+  });
+
+  if (!fs.existsSync("public/")) {
+    fs.mkdirSync("./public/" /* { recursive: true } */);
+  }
+
+  fs.writeFileSync("./public/index.html", renderedHTML);
 }
 
 main();
