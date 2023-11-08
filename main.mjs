d528c57f069cd094d72331c2bd6b273e5fa505ad
HicaroD <hdanrlley1@gmail.com>
2023-11-07 23:04:25 -0300

    chore: adding initial implementations

--- main.mjs
+++ main.mjs
@@ -5,7 +5,7 @@ import ejs from "ejs";
 
 function generateHTMLFiles(renderedHTML) {
   if (!fs.existsSync("public/")) {
-    fs.mkdirSync("./public/" /* { recursive: true } */);
+    fs.mkdirSync("./public/" /*, { recursive: true } */);
   }
 
   fs.writeFileSync("./public/index.html", renderedHTML);
@@ -15,11 +15,17 @@ function generateHTMLFiles(renderedHTML) {
 function main() {
   const layout = fs.readFileSync("./layouts/default.ejs").toString();
 
-  const renderedHTML = ejs.render(layout, {
-    site: { title: "website" },
-  });
+  // TODO: read from file
+  const config = {
+    site: { title: "Hicaro's website" },
+    profile: {
+      name: "Hícaro Dânrlley",
+      role: "Software engineer",
+    },
+  };
+  const renderedHTML = ejs.render(layout, config);
 
-  generateHTMLFiles(renderedHTML)
+  generateHTMLFiles(renderedHTML);
 }
 
 main();
