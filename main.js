18615ba9d744a6b2402147d00888258af358aaa3
HicaroD <hdanrlley1@gmail.com>
2023-11-16 10:16:34 -0300

    refactor(architecture): new file with config related code

--- main.js
+++ main.js
@@ -2,58 +2,9 @@ import fs from "fs";
 import path from "path";
 import ejs from "ejs";
 
-const CONFIG_FILE = "./config.json";
-const PROD_PUBLIC_DIR = "./public";
-const LOCAL_PUBLIC_DIR = "./_public";
+import { getEnvironmentSetup, getConfig, getPublicDirPath } from "./config.js";
+
 const PARTIALS_DIR = "./partials";
-const ASSETS_DIR = "./assets";
-
-function getPublicDirPath(environment) {
-  return environment === "prod" ? PROD_PUBLIC_DIR : LOCAL_PUBLIC_DIR;
-}
-
-function buildPublicDir(environment) {
-  const publicDir = getPublicDirPath(environment);
-  fs.mkdirSync(publicDir);
-  fs.cpSync(ASSETS_DIR, `${publicDir}/${ASSETS_DIR}`, { recursive: true });
-}
-
-function getGeneratorConfig(environment) {
-  const cwd = process.cwd();
-
-  return {
-    metaconfig: {
-      home_path: environment === "prod" ? "/" : "index.html",
-      topbar: [
-        {
-          item: "Resume",
-          path: environment === "prod" ? "resume" : "resume.html",
-        },
-        {
-          item: "Projects",
-          path: environment === "prod" ? "projects" : "projects.html",
-        },
-        { item: "Blog", path: environment === "prod" ? "blog" : "blog.html" },
-        { item: "CV", path: environment === "prod" ? "cv" : "cv.html" },
-      ],
-      paths: {
-        layouts: {
-          topbar: `../layouts/topbar`,
-          start: `${cwd}/layouts/start`,
-          end: `${cwd}/layouts/end`,
-        },
-        partials: {
-          home: `${cwd}/partials/home`,
-        },
-        css: [
-          "assets/css/globals.css",
-          "assets/css/topbar.css",
-          "assets/css/home.css",
-        ],
-      },
-    },
-  };
-}
 
 function generateHTMLFiles(renderedHTMLFiles, environment) {
   const publicDir = getPublicDirPath(environment);
@@ -97,26 +48,6 @@ function renderHTMLFiles(config) {
   }
 
   return renderedHTMLs;
-}
-
-function getConfig(environment) {
-  const userConfig = fs.readFileSync(CONFIG_FILE).toString();
-  const userConfigJson = JSON.parse(userConfig);
-  const generatorConfig = getGeneratorConfig(environment);
-  return Object.assign(userConfigJson, generatorConfig);
-}
-
-function getEnvironmentSetup() {
-  const environment = process.env.ENVIRON;
-  if (environment === undefined) {
-    throw new Error(
-      "Consider setting an environment variable, such as 'ENVIRON=prod' or 'ENVIRON=local'",
-    );
-  }
-  if (environment !== "prod" && environment !== "local") {
-    throw new Error(`Invalid value for ENVIRON variable: '${environment}'`);
-  }
-  return environment;
 }
 
 function main() {
